9146150595e36a717148bf4bf076111b192e6d6b
lixunsong <lixunsonghcl@126.com>
2022-01-12 11:46:51 +0800

    Add benchmark-init.

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -45,12 +45,18 @@
 
 
 
+(cl-letf (((symbol-function 'define-obsolete-function-alias) #'defalias))
+  (use-package benchmark-init
+    :config
+    (require 'benchmark-init-modes)                                     ; explicitly required
+    (add-hook 'after-init-hook #'benchmark-init/deactivate)))
+
 
 ;; (use-package benchmark-init
-;;   :ensure t
-;;   :config
-;;   ;; To disable collection of benchmark data after init is done.
-;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
+  ;; :ensure t
+  ;; :config
+  ;; To disable collection of benchmark data after init is done.
+  ;; (add-hook 'after-init-hook 'benchmark-init/deactivate))
 
 (provide 'init-packages)
 
