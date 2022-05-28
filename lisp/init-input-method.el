7f8e2a60c107ee1fcb673c1d7cf003734dd9304e
lixunsong <lixunsonghcl@126.com>
2022-05-28 16:42:38 +0800

    remove emacs-rime configs.

--- lisp/init-input-method.el
+++ lisp/init-input-method.el
@@ -2,48 +2,6 @@
 (require 'init-custom)
 (require 'init-funcs)
 
-;; 由于 rime 不能记忆用户自造词，所以切换回了 pyim
-(use-package rime  
-  :defer t
-  :disabled t
-  :custom
-  (default-input-method "rime")
-  :bind
-  (:map rime-active-mode-map
-   ("<tab>" . 'rime-inline-ascii)
-   :map rime-mode-map
-   ("C-`" . 'rime-send-keybinding)    ;; <---- 
-   ;; ("M-j" . 'rime-force-enable)
-   ) 
-  :config
-  (setq default-input-method "rime"
-	rime-show-candidate 'posframe)
-  ;; (setq rime-librime-root "~/.emacs.d/librime/dist")
-  (setq rime-user-data-dir "~/.emacs.d/rime/")
-  (setq rime-disable-predicates
-        '(rime-predicate-evil-mode-p
-          rime-predicate-after-alphabet-char-p
-          rime-predicate-space-after-cc-p
-	  rime-predicate-hydra-p
-          rime-predicate-punctuation-after-space-cc-p
-	  rime-predicate-punctuation-after-ascii-p
-          rime-predicate-prog-in-code-p
-	  rime-predicate-in-code-string-p
-	  rime-predicate-punctuation-line-begin-p
-	  ;; rime-predicate-space-after-ascii-p
-	  rime-predicate-ace-window-p
-          rime-predicate-current-uppercase-letter-p
-	  ;; rime-predicate-helm-mode-p
-          ))
-  (setq mode-line-mule-info '((:eval (rime-lighter))))
-  (setq rime-posframe-properties
-      (list :background-color "#333333"
-            :foreground-color "#dcdccc"
-            ;; :font "WenQuanYi Micro Hei Mono-14"
-            :internal-border-width 10))
-  ;; (add-hook 'find-file-hook #'toggle-input-method
-  )
-
 (use-package pyim
   :init
   (setq default-input-method "pyim")
@@ -71,7 +29,7 @@
               '(pyim-probe-punctuation-line-beginning
                 pyim-probe-punctuation-after-punctuation))
   (setq pyim-dicts
-	'((:name "sougou_base" :file "~/.emacs.d/pyim/sougou_base.pyim"))
+	`((:name "sougou_base" :file ,(concat lxs-home-dir".emacs.d/pyim/sougou_base.pyim")))
 	pyim-enable-shortcode nil)
   ;; ivy 拼音搜索
   (defun eh-ivy-cregexp (str)
