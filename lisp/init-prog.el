89c0f26e3d732b47f80bc02bafce93a0f9972c4c
lixunsong <lixunsonghcl@126.com>
2022-07-30 02:48:56 +0800

    feat(company, prog): toggle to use lsp-bridge or not.

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -7,14 +7,38 @@
   (setq lsp-bridge-enable-diagnostics nil)
   (add-hook 'python-mode-hook #'lsp-bridge-mode)
   (define-key acm-mode-map "\M-j" #'(lambda () (interactive) (acm-hide) (hydra-reading/body)))
-  (define-key acm-mode-map "\M-k" #'(lambda () (interactive) (acm-hide) (pyim-convert-string-at-point))))
+  (define-key acm-mode-map "\M-k" #'(lambda () (interactive) (acm-hide) (pyim-convert-string-at-point)))
+
+  (defvar xs-lsp-bridge-mode-enabled t)
+  (defun toggle-lsp-bridge-mode ()
+    (interactive)
+    (if xs-lsp-bridge-mode-enabled
+	(progn
+	  (call-interactively #'lsp-bridge-kill-process)
+	  (setq lsp-bridge-mode nil)
+	  (remove-hook 'python-mode-hook #'lsp-bridge-mode)
+	  (setq xs-lsp-bridge-mode-enabled nil)
+	  (global-company-mode t)
+	  (define-key prog-mode-map "\M-." #'xref-find-definitions)
+	  (define-key prog-mode-map "\M-," #'xref-pop-marker-stack)
+	  (message "lsp-bridge closed")
+	  )
+      (global-company-mode nil)
+      (add-hook 'python-mode-hook #'lsp-bridge-mode)
+      (setq xs-lsp-bridge-mode-enabled t)
+      (define-key prog-mode-map "\M-." #'lsp-bridge-find-def)
+      (define-key prog-mode-map "\M-," #'lsp-bridge-return-from-def)
+      (message "lsp-bridge enabled")))
+  )
 
 (use-package eglot
   :ensure t
   :config
   (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
   (setq read-process-output-max (* 1024 1024))
-  (setq eldoc-echo-area-use-multiline-p nil))
+  (setq eldoc-echo-area-use-multiline-p nil)
+  (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
+  )
 
 ;; python
 (use-package pyvenv
